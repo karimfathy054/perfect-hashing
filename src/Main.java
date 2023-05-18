57e4f34950ccd259a49f83b4a8ce967fc8a63fab
Karim Fathy <karim.fathy054@gmail.com>
2023-05-18 15:43:36 +0300

    creating a repo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int x = "hello world".hashCode();
+        int y = "dlrow olleh".hashCode();
+        int z = "aaaa".hashCode();
+        int s = "aaaaa".hashCode();
+
+    }
+}
