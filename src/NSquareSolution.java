eab078e89c2221a555fcad5165fdcfd78c43a2bc
Karim Fathy <karim.fathy054@gmail.com>
2023-05-18 19:15:01 +0300

    fixing rehashing + small testing

--- src/NSquareSolution.java
+++ src/NSquareSolution.java
@@ -38,13 +38,13 @@ public class NSquareSolution<T> {
             return false;
         }
         else{//collision
-            while(!rehash()){
+            while(!rehashAndInsert(entry)){
                 rehashings +=1;
             }
         }
         return true;
     }
-    boolean rehash(){
+    boolean rehashAndInsert(Entry e){
         Entry[] temp = new Entry[this.tableSize];
         this.hasher = new UniversalHasher(this.hashMatrixSize,this.keyBitSize);
         for (int i = 0; i < this.tableSize; i++) {
@@ -57,6 +57,13 @@ public class NSquareSolution<T> {
             else{//collision
                 return false;
             }
+        }
+        int hashingIndex = hasher.getHashedindex(e.key);
+        if(temp[hashingIndex]==null){//no collision
+            temp[hashingIndex]= e;
+        }
+        else{//collision for the new entry
+            return false;
         }
         this.table= temp;
         return true;
@@ -78,4 +85,43 @@ public class NSquareSolution<T> {
         return false;
     }
 
+    // public static void main(String[] args) {
+    //     String[] values = {"vsdhvbjh","vfdhnihnsdkjo","aseowjimnci","ckhbiashion"};
+    //     int[] keys = new int[values.length];
+    //     int i =  0;
+    //     for (String string : values) {
+    //         keys[i++]= string.hashCode();
+    //     }
+    //     NSquareSolution<String> hashtable = new NSquareSolution<>(2, 32);
+    //     for (int j = 0; j < keys.length; j++) {
+    //         hashtable.insert(keys[j],values[j]);
+    //     }
+    //     Scanner sc = new Scanner(System.in);
+    //     boolean flag = true;
+    //     while (flag){
+    //         System.out.println("choose input");
+    //         int input = sc.nextInt();
+    //         sc.nextLine();
+    //         switch (input) {
+    //             case 1:
+    //             System.out.println("delete ?");
+    //             String st = sc.nextLine();
+    //             int key =  st.hashCode(); 
+    //             System.out.println(hashtable.delete(key, st));
+    //                 break;
+    //         case 2:
+    //             flag = false;
+    //             default:
+    //             flag = false;
+    //                 break;
+    //         }
+    //     }
+    //     while(true){
+    //         System.out.println("search for??");
+    //         String st = sc.nextLine();
+    //         int key =  st.hashCode();
+    //         System.out.println(hashtable.search(key, st));
+    //     }
+    //     // System.out.println("kimo");
+    // }
 }
